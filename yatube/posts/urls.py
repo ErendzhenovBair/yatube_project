ec8f6473ed39e93b303350a993d4b40c5c873bf3
Bair Erendzhenov <erendzhenovbair1990@yandex.ru>
2023-02-08 21:59:54 +0300

    1 commit

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('group/<slug:slug>/', views.group_posts), 
-    path('', views.index) # главная страница
+    path('', views.index, name = 'index'), # главная страница
+    path('group_list/<slug:slug>/', views.group_posts, name = 'group_list'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,34 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
 
+
+from .models import Group, Post
+
+from yatube.settings import NUMBER_OF_POSTS
 
 #Главная страница
 def index(request):    
-    return HttpResponse('Дневники твоих любимых блогеров')
-
+    posts = Post.objects.order_by('-pub_date')[:NUMBER_OF_POSTS]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)    
+    #return HttpResponse Главная страница
 
 # Страница с постами, отфильтрованными по группам
 
 def group_posts(request, slug):
-    return HttpResponse(f'Здесь будут посты {slug}, отфильтрованные по группам')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:NUMBER_OF_POSTS]
+    title = f'Записи сообществ {group}'
+    text = Group.description
+    context = {
+        'group': group,
+        'posts': posts,
+        'text': text,
+    }
+    return render(request, 'posts/group_posts.html', context) 
+       
+    
 
 
